5d370e4df1530ad72854d4598f7c67bda2e9fcd3
heily-tech <edelbteen@gmail.com>
2025-09-20 17:31:09 +0900

    feat: simple todo app 구현
    - 새로운 todo 추가 기능
    - 체크박스 완료/해제 처리
    - 편집(edit) 및 삭제(remove) 버튼 추가
    - 입력값 수정 후 blur 시 자동 비활성화
    - 이벤트 리스너를 createTodoElement 내부로 이동 (closure)

---
+++ js/04_todo.js
@@ -0,0 +1,85 @@
+const list = document.getElementById("list");
+const createBtn = document.getElementById("create-btn");
+
+let todos = [];
+
+/* todo 추가하기 */
+createBtn.addEventListener('click', createNewTodo);
+function createNewTodo() {
+    const item = {
+        id: new Date().getTime(),
+        text: "",
+        complete: false
+    }
+    todos.unshift(item);
+
+    const {itemEl, inputEl} = createTodoElement(item);
+    list.prepend(itemEl);
+
+    inputEl.removeAttribute("disabled");
+    inputEl.focus();
+}
+function createTodoElement(item) {
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.complete;
+
+    if (item.complete) {
+        itemEl.classList.add("complete");
+    }
+
+    const inputEl = document.createElement("input");
+    inputEl.type = "text";
+    inputEl.value = item.text;
+    inputEl.setAttribute("disabled", "");
+
+    const actionsEl = document.createElement("div");
+    actionsEl.classList.add("actions");
+
+    const editBtnEl = document.createElement("button");
+    editBtnEl.classList.add("material-icons");
+    editBtnEl.innerText = "edit";
+
+    const removeBtnEl = document.createElement("button");
+    removeBtnEl.classList.add("material-icons", "remove-btn");
+    removeBtnEl.innerText = "remove_circle";
+
+    actionsEl.append(editBtnEl, removeBtnEl);
+    itemEl.append(checkbox, inputEl, actionsEl);
+    
+    /* 이벤트 리스너 관리 */
+    checkbox.addEventListener("change", () => {
+        item.complete = checkbox.checked;
+        itemEl.classList.toggle("complete", item.complete);
+        // if (item.complete) {
+        //     itemEl.classList.add("complete");
+        // } else {
+        //     itemEl.classList.remove("complete");
+        // }
+    });
+
+    inputEl.addEventListener("input", () => {
+        item.text = inputEl.value;
+    });
+
+    inputEl.addEventListener("blur", () => {
+        inputEl.setAttribute("disabled", "");
+    });
+
+    editBtnEl.addEventListener("click", () => {
+        inputEl.removeAttribute("disabled");
+        inputEl.focus();
+    });
+
+    removeBtnEl.addEventListener("click", () => {
+        todos = todos.filter(t => t.id != item.id);
+        itemEl.remove();
+    });
+
+    return { itemEl, inputEl };
+}
+
+/* 로컬 스토리지 설정 */
