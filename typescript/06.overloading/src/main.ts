9fdc5fe97691ebd9776038523b2ca539b9b53fc0
heily-tech <edelbteen@gmail.com>
2025-10-21 23:36:58 +0900

    feat: function overloading

---
+++ typescript/06.overloading/src/main.ts
@@ -0,0 +1,34 @@
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+
+function add(a: any, b: any): any {
+    return a + b;
+}
+
+add("hello", "world");
+add(1, 1);
+
+
+
+function saySomething(word: string | string[]): string {
+    if (typeof word === "string")
+        return word;
+    else if (Array.isArray(word))
+        return word.join(" ");
+
+    throw new Error("unable to say something.");
+}
+saySomething(['hello', 'world']);  // 'hello'
+
+/* function overloading */
+function saySomething2(word: string): string;
+function saySomething2(words: string[]): string;
+function saySomething2(word: any): any {
+    if (typeof word === "string")
+        return word;
+    else if (Array.isArray(word))
+        return word.join(" ");
+
+    throw new Error("unable to say something.");
+}
+saySomething(['hello', 'world']);  // 'hello'
