e63b6ad9cf360c6314608ea37aae74ff0b6b553a
heily-tech <edelbteen@gmail.com>
2025-10-23 16:42:01 +0900

    refactor: divide lists component

--- react/react-todo-app/hooks-react-todo-app/src/App.jsx
+++ react/react-todo-app/hooks-react-todo-app/src/App.jsx
@@ -1,5 +1,7 @@
 import { Component, useState } from 'react';
 import './App.css'
+import Lists from './components/Lists';
+import Form from './components/Form';
 
 // function App() {
 //   return (
@@ -24,56 +26,22 @@ export default function App() {
       completed: false,
     }
   ]);
-  const [value, setValue] = useState('');
-  
-  const btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '4px 9px',
-    borderRadius: '31%',
-    cursor: 'pointer',
-    float: 'right'
-  }
 
-  const getStyle = (completed) => {
-    return {
-      padding: '7px',
-      borderBottom: '1px rgba(197, 224, 226, 1) dotted',
-      textDecoration: completed ? 'line-through' : 'none'
-    }
-  }
 
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id)
-    setTodoData(newTodoData);
-  }
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setValue(e.target.value);
-  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+      e.preventDefault();
 
-    let newTodo = {
+      let newTodo = {
       id: Date.now(),
       title: value,
       completed: false
-    }
-    setTodoData([...todoData, newTodo]);
-    setValue('');
+      }
+      setTodoData([...todoData, newTodo]);
+      setValue('');
   }
+  const [value, setValue] = useState('');
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id)
-        data.completed = !data.completed;
-      return data;
-    })
-
-    setTodoData(newTodoData);
-  }
 
   return (
     <div className='container'>
@@ -81,20 +49,15 @@ export default function App() {
         <div className='title'>
           <h1>할 일 목록</h1>
         </div>
-        {todoData.map(
-          (data) => (
-            <div style={getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" checked={data.completed} onChange={() => handleCompleteChange(data.id)}/>
-              {data.title}
-              <button style={btnStyle} onClick={() => handleClick(data.id)}>X</button>
-            </div>
-          )
-        )}
-        <form style={ {display: 'flex'} } onSubmit={handleSubmit}>
-          <input type='text' name='value' style={ {flex: '10', padding: '5px'} } 
-                  placeholder='Write to do.' value={value} onChange={handleChange}/>
-          <input type='submit' value='Submit' className='btn' style={ {flex: '1'}} />
-        </form>
+        <Lists 
+          todoData={todoData}
+          setTodoData={setTodoData}
+        />
+        <Form
+          handleSubmit={handleSubmit}
+          value={value}
+          setValue={setValue}
+        />
       </div>
     </div>
   )
