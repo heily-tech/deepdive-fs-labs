dd0abe171c84d58aa960ea8ee6a7d341f10d1c40
heily-tech <edelbteen@gmail.com>
2025-10-23 23:13:51 +0900

    feat: nav bar struct

--- react/react-netflix/react-netflix/src/components/Nav.js
+++ react/react-netflix/react-netflix/src/components/Nav.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
 
 export default function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {  // 임계값까지 스크롤 내리면 배경이 Black
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+
   return (
-    <nav>
+    <nav className={`nav ${show && "nav__black"} `}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
             alt="Netflix logo"
             className="nav__logo"
