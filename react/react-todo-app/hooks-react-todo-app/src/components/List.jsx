325004cf3001f62b0c32ea0cd805019d3b394577
heily-tech <edelbteen@gmail.com>
2025-10-23 21:15:36 +0900

    feat: add list update

--- react/react-todo-app/hooks-react-todo-app/src/components/List.jsx
+++ react/react-todo-app/hooks-react-todo-app/src/components/List.jsx
@@ -1,4 +1,10 @@
+import {useState} from "react";
+
+
 export default function List({id, title, completed, todoData, setTodoData}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const getStyle = (completed) => {
         return {
             padding: '7px',
@@ -30,9 +36,45 @@ export default function List({id, title, completed, todoData, setTodoData}) {
         setTodoData(newTodoData);
     }
 
-    return <div style={getStyle(completed)}>
+    const handleEditChange = (e) => {
+        setEditedTitle(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTodoData = todoData.map((data) => {
+            if (data.id === id) {
+                data.title = editedTitle
+            }
+            return data;
+        })
+
+        setTodoData(newTodoData);
+        setIsEditing(false);
+    }
+
+    if (isEditing) {
+        return (
+            <div>
+                <form style={getStyle(completed)} onSubmit={handleSubmit}>
+                    <input
+                        value={editedTitle}
+                        autoFocus
+                        onChange={handleEditChange}
+                    />
+                    <button type="button" style={btnStyle} onClick={() => setIsEditing(false)}>X</button>
+                    <button type="submit" style={btnStyle}>Save</button>
+                </form>
+            </div>
+        )
+    } else {
+        return <div style={getStyle(completed)}>
                 <input type="checkbox" checked={completed} onChange={() => handleCompleteChange(id)}/>
                 {title}
                 <button style={btnStyle} onClick={() => handleClick(id)}>X</button>
+                <button style={btnStyle} onClick={() => setIsEditing(true)}>Edit</button>
             </div>
+    }
+
+    
 }
