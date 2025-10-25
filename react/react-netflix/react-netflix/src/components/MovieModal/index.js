08a55904a7c7df3e152a5b079119b3cf739ec17b
heily-tech <edelbteen@gmail.com>
2025-10-25 18:08:31 +0900

    feat: decide modal boundary action with useRef

--- react/react-netflix/react-netflix/src/components/MovieModal/index.js
+++ react/react-netflix/react-netflix/src/components/MovieModal/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./MovieModal.css";
+import useOnClickOutSide from '../../hooks/useOnClickOutSide';
 
 function MovieModal({
     backdrop_path,
@@ -11,29 +12,30 @@ function MovieModal({
     vote_average,
     setModalOpen
 }) {
+    const ref = useRef();
+    useOnClickOutSide(ref, () => {setModalOpen(false)});
 
-
-  return (
-    <div className="presentation">
-        <div className="wrapper-modal">
-            <div className="modal">
-                <span className="modal-close" onClick={() => setModalOpen(false)}>X</span>
-                <img
-                    className="modal__poster-img"
-                    src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-                    alt="modal__poster-img" />
-                <div className="modal__content">
-                    <p className="modal__details">
-                        <span className="modal__user-perc">100% for you</span>{" "}{release_date ? release_date : first_air_date}
-                    </p>
-                    <h2 className="modal__title">{title ? title : name}</h2>
-                    <p className="modal__overview">평점 : {vote_average}</p>
-                    <p className="modal__overview"> {overview}</p>
+    return (
+        <div className="presentation">
+            <div className="wrapper-modal">
+                <div className="modal" ref={ref}>
+                    <span className="modal-close" onClick={() => setModalOpen(false)}>X</span>
+                    <img
+                        className="modal__poster-img"
+                        src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+                        alt="modal__poster-img" />
+                    <div className="modal__content">
+                        <p className="modal__details">
+                            <span className="modal__user-perc">100% for you</span>{" "}{release_date ? release_date : first_air_date}
+                        </p>
+                        <h2 className="modal__title">{title ? title : name}</h2>
+                        <p className="modal__overview">평점 : {vote_average}</p>
+                        <p className="modal__overview"> {overview}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default MovieModal
