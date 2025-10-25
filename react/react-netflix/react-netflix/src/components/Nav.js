6aa04c0db5962e502e037b91439b8ad39156c371
heily-tech <edelbteen@gmail.com>
2025-10-25 17:22:06 +0900

    feat: search movies with debounced term

--- react/react-netflix/react-netflix/src/components/Nav.js
+++ react/react-netflix/react-netflix/src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,13 +21,25 @@ export default function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   return (
     <nav className={`nav ${show && "nav__black"} `}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
             alt="Netflix logo"
             className="nav__logo"
-            onClick = {() => (window.location.reload())} />
+            onClick = {() => navigate("/")} />
+
+        <input 
+          className="nav__input" 
+          type="text" 
+          value={searchValue} 
+          placeholder="Title, Actor, Genre"
+          onChange={handleChange} />
+        
         <img src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
             alt="User logged"
             className="nav__avatar" />
