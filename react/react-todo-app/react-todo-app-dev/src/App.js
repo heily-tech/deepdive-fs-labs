4e24368e6d11977f4710a740cc919728e0089479
heily-tech <edelbteen@gmail.com>
2025-11-03 11:54:16 +0900

    feat: list todos

--- react/react-todo-app/react-todo-app-dev/src/App.js
+++ react/react-todo-app/react-todo-app-dev/src/App.js
@@ -2,6 +2,61 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  btnStyle = {
+    color: "#235373",
+    border: "none",
+    padding: "2px 5px",
+    borderRadius: "30%",
+    cursor: "pointer",
+    float: "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "20px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    }
+  }
+
+  todoData = [
+    {
+      id: "1",
+      title: "Study english",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "Cleaning my desk",
+      completed: false
+    },
+    {
+      id: "3",
+      title: "Solve 1 algorithm problem",
+      completed: true
+    },
+    {
+      id: "4",
+      title: "Test Data",
+      completed: true
+    }
+
+  ]
+  /*
+  ,
+    {
+      id: "4",
+      title: "Test Data",
+      completed: true
+    }
+  */
+
+
+  handleClick = (id) => {
+    let newTodoData = this.todoData.filter((data) => data.id !== id)
+    console.log(newTodoData);
+  }
+
   render() {
     return (
       <div className="container">
@@ -9,6 +64,17 @@ export default class App extends Component {
           <div className="title">
             <h1><i>To do.</i></h1>
           </div>
+
+        {this.todoData.map((data) => (
+          <div style={this.getStyle()} key={data.id}>
+            <input type="checkbox" defaultChecked={false} />
+            {" "}{data.title}
+            <button 
+              style={this.btnStyle} 
+              onClick={() => this.handleClick(data.id)}>X</button>
+          </div>
+        ))}
+
         </div>
       </div>
     )
