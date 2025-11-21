51c0fc90025e02886c7232f40e72ef671bd6d946
heily-tech <edelbteen@gmail.com>
2025-11-21 17:52:23 +0900

    CGLIB 실습

--- java/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ java/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -20,4 +20,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName,itemPrice, discountPrice);
     }
+
+    // Test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
