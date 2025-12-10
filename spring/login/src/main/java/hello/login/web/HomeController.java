b4b19f01eb527f345f7105555178b64c6244e689
heily-tech <edelbteen@gmail.com>
2025-12-10 19:20:28 +0900

    feat: add httpservlet for session control

--- spring/login/src/main/java/hello/login/web/HomeController.java
+++ spring/login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 @Slf4j
 @Controller
@@ -41,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         Member member = (Member) sessionManager.getSession(request);
@@ -51,6 +54,34 @@ public class HomeController {
             return "home";
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null)
+            return "home";
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // Login
+        if (loginMember == null)
+            return "home";
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // Login
+        if (loginMember == null)
+            return "home";
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
