fc6b5f0b7630a3b40c16480b8c5d4d98202e9e3d
heily-tech <edelbteen@gmail.com>
2025-12-18 16:46:31 +0900

    test: member repository connection - spring db

--- spring_db/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ spring_db/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,19 +1,42 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.*;
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-public class MemberRepositoryV0Test {
+class MemberRepositoryV1Test {
 
-    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    MemberRepositoryV1 repository;
 
+    @BeforeEach
+    void beforeEach() {
+        // DriverManager 방식
+        // 항상 새로운 커넥션 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // Connection Pooling 방식
+        // HikariProxyConnection -> JdbcConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+ 
     @Test
     void crud() throws SQLException {
         // save
