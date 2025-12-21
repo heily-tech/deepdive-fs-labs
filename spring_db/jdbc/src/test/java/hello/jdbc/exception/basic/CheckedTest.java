69d34178a12bc6a7f85da6fcf2d210d55f31e6d0
heily-tech <edelbteen@gmail.com>
2025-12-21 17:42:08 +0900

    test: checked exception - spring db

---
+++ spring_db/jdbc/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,55 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyCheckedException.class);
+    }
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String msg) {
+            super(msg);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        /* 예외를 잡아서 처리 */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                // 예외 처리 로직
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /* 예외를 밖으로 던짐 */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+
+}
